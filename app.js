9dcbf75a0c5990314f967341253e181555a5e702
Joseph McCarron <joe@zendesk.com>
2015-01-12 16:14:16 -0800

    require type field and fix failure notice

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
     defaultState: 'form',
     events: {
       'app.activated':'onAppActivated',
-      'click .submit_form':'onFormSubmitted'
+      'click .submit_form':'onFormSubmitted',
+      'postTicket.fail':'onSubmitFailed'
     },
 
     requests: {
@@ -21,6 +22,7 @@
     },
 
     onAppActivated: function(e) {
+
     },
     onFormSubmitted: function(e) {
       if(e) {e.preventDefault();}
@@ -39,11 +41,18 @@
       var tag = this.$('.tag').val();
       var ticketRaw = {'ticket':{'subject':comment,'comment': { 'body': comment }, 'tags':[tag || 'feedback', ratingTag]}};
       var ticketString = JSON.stringify(ticketRaw);
-
-      this.ajax('postTicket', ticketString).done( function(response) {
-        services.notify(this.setting('Success_notice') || 'Your feedback has been submitted, thanks!');
-        this.switchTo('form');
-      });
+      if(tag) {
+        this.ajax('postTicket', ticketString).done( function(response) {
+          services.notify(this.setting('Success_notice') || 'Your feedback has been submitted, thanks!');
+          this.switchTo('form');
+        });
+      } else {
+        this.missingField("Feedback type");
+      }
+      
+    },
+    missingField: function(field) {
+      services.notify('You missed the required field "' + field + '" :( Try again?', 'error');
     },
     onSubmitFailed: function(response) {
       services.notify( this.setting('Fail_notice') || 'Failed to submit your feedback, sorry! Try again? The response has been logged to the console.', 'error');
