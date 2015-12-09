c40a62cfbd456806b9e0e5fd9b6c8ea6881ba27a
Jeremiah Currier <jeremiah.currier@gmail.com>
2015-12-09 15:03:55 -0800

    Streamlined & ready for the Marketplace

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # App name
 
-[brief description of the app]
+*An app for receiving feedback and/or survey sending to evaluate agent happiness.*
+
+A ticket is created in your Zendesk via the feedback given by your agents in this app. Agents use the app while working tickets.
 
 ### The following information is displayed:
 
-* info1
-* info2
-* info3
+1. Instruction area
+ - Instructions are configurable in app settings
+2. Two drop-downs each with two options
+ - 'Type'
+ - - options/tags are configurable in app settings
+ - 'Rating' - configurable but default title value is 'Rating'
+ - - options/tags are configurable in app settings
+3. Textarea for the body of the feedback comment
 
-Please submit bug reports to [Insert Link](). Pull requests are welcome.
+
+Please submit bug reports to [Success Apps](mailto:bugs@successapps.zendesk.com). Pull requests are welcome.
 
 ### Screenshot(s):
-[put your screenshots down here.]
+
+![](http://g.recordit.co/zTlJIeHyad.gif)
--- app.js
+++ app.js
@@ -1,64 +1,85 @@
 (function() {
 
   return {
+    
     defaultState: 'form',
+
     events: {
-      'app.activated':'onAppActivated',
-      'click .submit_form':'onFormSubmitted',
-      'postTicket.fail':'onSubmitFailed'
+      // Framework Events
+      'app.activated'      :'onAppActivated',
+      // DOM Events
+      'click .submit_form' :'onFormSubmitted',
+      'click .toggle-app'  : 'toggleAppContainer'
     },
 
     requests: {
       postTicket: function (ticket) {
         return {
-          url: '/api/v2/tickets.json',
-          type: 'POST',
-          dataType: 'JSON',
-          contentType: 'application/JSON',
-          proxy_v2: true,
-          data: ticket
+          url         : '/api/v2/tickets.json',
+          type        : 'POST',
+          dataType    : 'JSON',
+          contentType : 'application/JSON',
+          data        : JSON.stringify(ticket)
         };
       }
     },
 
-    onAppActivated: function(e) {
-
+    onAppActivated: function() {
+      this.$('.app-container').hide();
+      this.$('.toggle-app i').prop('class', 'icon-plus');
     },
-    onFormSubmitted: function(e) {
-      if(e) {e.preventDefault();}
 
-      var comment = this.$('.feedback').val();
-      var rating = this.$('.rating').val();
-      var ratingPrefix = this.setting('Rating_tag_prefix');
-      var ratingTag;
-      if(rating == 'good' || rating == 'bad') {
-        if(ratingPrefix) {
+    onFormSubmitted: function(e) {
+      if (e) {
+        e.preventDefault();
+      }
+      var comment      = this.$('.feedback').val(),
+          rating       = this.$('.rating').val(),
+          ratingPrefix = this.setting('Rating_tag_prefix'),
+          ratingTag;
+      if (rating === 'good' || rating === 'bad') {
+        if (ratingPrefix) {
           ratingTag = ratingPrefix + '_' + rating;
         } else {
           ratingTag = 'feedback_' + rating;
         }
       }
-      var tag = this.$('.tag').val();
-      var ticketRaw = {'ticket':{'subject':comment,'comment': { 'body': comment }, 'tags':[tag || 'feedback', ratingTag]}};
-      var ticketString = JSON.stringify(ticketRaw);
-      if(tag) {
-        this.ajax('postTicket', ticketString).done( function(response) {
-          services.notify(this.setting('Success_notice') || 'Your feedback has been submitted, thanks!');
-          this.switchTo('form');
-        });
+      var tag    = this.$('.tag').val(),
+          ticket = {
+            'ticket' : {
+              'subject' : comment,
+              'comment' : { 'body' : comment },
+              'tags'    : [tag || 'feedback', ratingTag]
+            }
+          };
+      if (rating !== null && tag !== null && comment !== '') {
+        this.ajax('postTicket', ticket)
+          .done( function(response) {
+            services.notify('Your feedback has been submitted, thanks!', 'notice');
+            console.log('Created Ticket ID: {' + response.ticket.id + '} in this Zendesk account');
+            this.switchTo('form');
+          })
+          .fail( function(response){
+            services.notify('Failed to submit your feedback, sorry! Try again? The response has been logged to the console.', 'error');
+            this.switchTo('form');
+          });
       } else {
-        this.missingField("Feedback type");
+        services.notify('<p>One of the following is missing:</p><ol><li>Type</li><li>Rating</li><li>Comment</li></ol>', 'error');
       }
-      
     },
-    missingField: function(field) {
-      services.notify('You missed the required field "' + field + '" :( Try again?', 'error');
-    },
-    onSubmitFailed: function(response) {
-      services.notify( this.setting('Fail_notice') || 'Failed to submit your feedback, sorry! Try again? The response has been logged to the console.', 'error');
-      console.log(response);
-      
+
+    toggleAppContainer: function(){
+      var $container = this.$('.app-container'),
+          $icon      = this.$('.toggle-app i');
+      if ($container.is(':visible')){
+        $container.hide();
+        $icon.prop('class', 'icon-plus');
+      } else {
+        $container.show();
+        $icon.prop('class', 'icon-minus');
+      }
     }
+
   };
 
 }());
